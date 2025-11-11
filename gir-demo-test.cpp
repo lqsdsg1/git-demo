d51541126ef73194692e556e4f73edf9dc86ae46
lqsdsg1 <lqsdsg1@gmail.com>
2025-11-11 17:33:12 +0800

    second save

--- gir-demo-test.cpp
+++ gir-demo-test.cpp
@@ -3,5 +3,7 @@
 int main() {
     std::cout << "Hello, world!" << std::endl;
     std::cout << "你好，世界！" << std::endl;
+    std::cout << "test-change" << std::endl;
+    std::cout << "third-test" << std::endl;
     return 0;
 }
